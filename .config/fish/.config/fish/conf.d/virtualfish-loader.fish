07ede663620b8840bc30f0d4bf9a04e383a6a1a2
abeer <abeersud@gmail.com>
2025-04-26 13:36:27 -0400

    added river config

--- .config/fish/.config/fish/conf.d/virtualfish-loader.fish
+++ .config/fish/.config/fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
 set -g VIRTUALFISH_VERSION 2.5.9
-set -g VIRTUALFISH_PYTHON_EXEC /home/abeer/anaconda3/bin/python
-source /home/abeer/anaconda3/lib/python3.12/site-packages/virtualfish/virtual.fish
+set -g VIRTUALFISH_PYTHON_EXEC /home/abeer/.local/share/pipx/venvs/virtualfish/bin/python
+source /home/abeer/.local/share/pipx/venvs/virtualfish/lib/python3.13/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
---
+++ .config/river/.config/river/scripts/monitors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wlr-randr --output DP-6  --pos 0,0
+wlr-randr --output HDMI-A-1 --pos 2560,0 --scale 1.25
